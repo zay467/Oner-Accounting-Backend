4533d74ae76f100703c551fa5689f0e4d1795330
Zay Min Maw <zayminmaw77@gmail.com>
2021-09-06 17:06:52 +0630

    removed unnecessary annotation

--- src/main/java/com/thamardaw/oner/entity/Accounts.java
+++ src/main/java/com/thamardaw/oner/entity/Accounts.java
@@ -3,8 +3,6 @@ package com.thamardaw.oner.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -30,7 +28,6 @@ public class Accounts {
     @Column
     private String description;
 
-    @Fetch(FetchMode.JOIN)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "categoryId")
     private Category category;
@@ -38,7 +35,6 @@ public class Accounts {
     @Column(insertable = false,updatable = false)
     private Long categoryId;
 
-    @Fetch(FetchMode.JOIN)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "accountTypeId")
     private AccountType accountType;
---
+++ src/main/java/com/thamardaw/oner/entity/BillItems.java
@@ -0,0 +1,2 @@
+package com.thamardaw.oner.entity;public class BillItems {
+}
