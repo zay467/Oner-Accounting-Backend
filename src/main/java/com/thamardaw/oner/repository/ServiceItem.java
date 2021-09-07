b4f65468415965525e46f12ec39a9c58e523b798
Zay Min Maw <zayminmaw77@gmail.com>
2021-09-07 20:59:11 +0630

    fix

--- src/main/java/com/thamardaw/oner/repository/ServiceItem.java
+++
@@ -1,6 +0,0 @@
-package com.thamardaw.oner.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceItem extends JpaRepository<ServiceItem,Long> {
-}
---
+++ src/main/java/com/thamardaw/oner/repository/ServiceItemRepository.java
@@ -0,0 +1,7 @@
+package com.thamardaw.oner.repository;
+
+import com.thamardaw.oner.entity.ServiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceItemRepository extends JpaRepository<ServiceItem,Long> {
+}
