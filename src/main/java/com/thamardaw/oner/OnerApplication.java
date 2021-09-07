cdb9b4fda6407af20dfa7539c84039631bd23c19
Zay Min Maw <zayminmaw77@gmail.com>
2021-09-07 19:25:21 +0630

    added bCryptPasswordEncoder bean

--- src/main/java/com/thamardaw/oner/OnerApplication.java
+++ src/main/java/com/thamardaw/oner/OnerApplication.java
@@ -2,6 +2,8 @@ package com.thamardaw.oner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class OnerApplication {
@@ -10,4 +12,8 @@ public class OnerApplication {
 		SpringApplication.run(OnerApplication.class, args);
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }
